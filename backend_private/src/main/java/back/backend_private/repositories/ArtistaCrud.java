a1ecbfa1e576b25ac6fde7ae9b8259c1567f1306
anicolau15191 <anicolau15191@alumnes.iesmanacor.cat>
2022-02-04 11:26:24 +0100

    sprint3

--- backend_private/src/main/java/back/backend_private/repositories/ArtistaCrud.java
+++ backend_private/src/main/java/back/backend_private/repositories/ArtistaCrud.java
@@ -1,7 +1,10 @@
 package back.backend_private.repositories;
 
 import back.backend_private.entity.Artista;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
 
 public interface ArtistaCrud extends CrudRepository<Artista,Integer> {
 }
