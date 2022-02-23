0addb4a028df802386f591c044a0ce30fd08c4dc
anicolau15191 <anicolau15191@alumnes.iesmanacor.cat>
2022-02-23 11:41:09 +0100

    conflict

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,8 @@
 import React, { Component } from 'react';
 import TabsGaleria from './Component/TabsGaleria';
 import Nav from "./Component/Nav"
+import Cercador from "./Component/Cercador";
+import Calendar from "./Component/Calendar";
 import Compra from './Component/Compra'
 import Exposicio from './Component/Exposicio'
 import './App.css'
@@ -19,6 +21,8 @@ class App extends Component {
       <div>
         <Router>
           <Route path="/" component={Nav} />
+          <Route path="/home" component={Cercador} />
+          <Route path="/calendari" component={Calendar} />
           <Route path="/Galeria/:idGaleria" component={TabsGaleria} />
           <Route path="/Exposicio/:nom/:id/:idGaleria" component={Exposicio} />
           <Route path="/Compra/:nom/:id/:idGaleria/:nomExpo/:idExpo" component={Compra} />
